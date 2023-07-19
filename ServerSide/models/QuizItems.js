fd3a1f09611908ec18377fec86a5864bcca433e2
eya26 <eya.chaftar26@gmail.com>
2023-07-20 01:45:47 +0200

    Authentication backen

---
+++ ServerSide/models/QuizItems.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+const User = require("./user")
+
+const QuizItemsSchema = new mongoose.Schema(
+    
+        {
+            author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+            quizItems:[
+                {
+                    answers:  [
+                        {
+                            "value": String,
+                            "Boolean": Boolean
+                        }, 
+                        {
+                            "value": String,
+                            "Boolean": Boolean
+                        },
+                        {
+                            "value": String,
+                            "Boolean": Boolean
+                        },
+                        {
+                            "value": String,
+                            "Boolean": Boolean
+                        }
+                    ]
+                    ,
+                    question: {
+                        type: String
+                    }
+                }
+            ]
+        }
+    
+);
+
+
+module.exports = mongoose.model("QuizItems", QuizItemsSchema);
