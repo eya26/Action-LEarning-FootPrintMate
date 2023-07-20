28def272e27b576b7a07d7d0e90aae0b3fb161b5
eya26 <eya.chaftar26@gmail.com>
2023-07-20 15:52:26 +0200

    Updated serverSide

--- ServerSide/controllers/user.js
+++ ServerSide/controllers/user.js
@@ -5,81 +5,76 @@ var jwt = require('jsonwebtoken')
 var expressJwt = require('express-jwt')
 
 
-const signup = (req,res) => {
-    
-    const errors = validationResult(req)
-    if(!errors.isEmpty()){
-        console.log(errors);
-
+const signup = async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
         return res.status(400).json({
-            error: errors.array()[0].msg
-        })
+          error: errors.array()[0].msg
+        });
+      }
+  
+      const { email, password } = req.body;
+  
+      const userExists = await User.findOne({ email });
+      if (userExists) {
+        return res.status(400).send("User already registered.");
+      }
+  
+      const user = new User(req.body);
+      const savedUser = await user.save();
+  
+      return res.json({
+        message: "Success",
+        user: savedUser
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Something went wrong");
     }
-    const { email, password } = req.body;
+  };
+  
 
-  try {
-    const user = new User(req.body)
-    if (user == User.findOne({email})) return res.status(400).send("User already registered.");
-    
-    user.save((err, user) => {
-        if(err) {
-
-            return res.status(400).json({
-                error:"Unable to add user"
-            
-            })
+  const signin = async (req, res) => {
+    const { user_email, password } = req.body;
+  
+    try {
+      const user = await User.findOne({ email: user_email });
+      if (!user) {
+        return res.status(400).json({
+          error: "Email was not found"
+        });
+      }
+  
+      //Authenticate the user
+      if (!user.authenticate(password)) {
+        return res.status(400).json({
+          error: "Email and password do not match"
+        });
+      }
+  
+      // Create token
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+  
+      // Put token into cookie
+      res.cookie('token', token, { expire: new Date() + 1 });
+  
+      // Send response
+      const { _id, name, email } = user;
+      return res.json({
+        user: {
+          _id,
+          name,
+          email,
         }
-        console.log(user)
-        return res.json({
-            message: "Success",
-            user
-        })
-    })
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Something went wrong");
-  }
- 
-};
-
-const signin = (req,res) => {
-    const {name,password} =req.body
-
-    User.findOne({name}, (err,user)=> {
-        if(err || !user) {
-            return res.status(400).json({
-                error:"Email was not found"
-            })
-              
-        }
-
-        //Authenticate the user
-        if(!user.authenticate(password)) {
-            console.log("error")
-            return res.status(400).json({
-                error: "Email and password do not match"
-            })
-        }
-
-        // Create token
-        const token = jwt.sign({_id: user._id}, process.env.SECRET)
-
-        // Put token into cookie
-        res.cookie('token',token, {expire: new Date()+1})
-
-        // Send response 
-        
-        const {_id,name,email,isAdmin,encry_password} = user
-        console.log(user.email)
-        return res.json({
-            user: {
-                _id,
-                name,
-                email,
-            }
-        })
-    })
-};
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).send("Something went wrong");
+    }
+  };
+  
+  
 
 const signout = (req,res) => {
     res.clearCookie("token")
